678768abe6a1c77502ff50d6d1b97dd9e12e8aa6
Thibault Béziers la Fosse <thiblf@gmail.com>
2018-09-19 12:32:11 +0200

    added an other test case verifying the Qualified name computation

---
+++ src/test/resources/classes/App.java
@@ -0,0 +1,16 @@
+package root;
+
+public class App {
+
+    public boolean foo() {
+        int i = 0;
+        int j = 1;
+        return i == j;
+    }
+
+    public boolean bar() {
+        int i = 0;
+        int j = 0;
+        return i != j;
+    }
+}
