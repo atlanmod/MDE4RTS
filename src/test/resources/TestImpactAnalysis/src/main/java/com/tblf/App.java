c074a880f90fc3791e1a436121056cc84ed0a344
Thibault Béziers la Fosse <thiblf@gmail.com>
2018-10-22 16:44:24 +0200

    updated the projects for testing

---
+++ src/test/resources/TestImpactAnalysis/src/main/java/com/tblf/App.java
@@ -0,0 +1,13 @@
+package com.tblf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends ParentClass
+{
+    @Override
+    public void method() {
+        System.out.println("Hello world !!!");
+    }
+}
---
+++ src/test/resources/TestRTSAllUpdates/src/main/java/com/tblf/AppChild.java
@@ -0,0 +1,11 @@
+package com.tblf;
+
+public class AppChild extends App {
+
+
+
+    @Override
+    public void methodThatWillBeOverriden() {
+        System.out.println("I'm overriding my parent. ");
+    }
+}
