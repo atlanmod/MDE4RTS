68cdfd410cc9913b8c356b06b3201e3d523f2e1c
Thibault Béziers la Fosse <thiblf@gmail.com>
2019-04-04 10:57:54 +0200

    Updated the premain class to perform instrumentation using bytebuddy

--- Agent/src/main/java/com/tblf/Agent.java
+++ Agent/src/main/java/com/tblf/Agent.java
@@ -1,6 +1,11 @@
 package com.tblf;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
+
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Method;
 
 /**
  * Hello world!
@@ -9,6 +14,24 @@ import java.lang.instrument.Instrumentation;
 public class Agent
 {
     public static void premain(String args, Instrumentation instrumentation) {
-        System.out.println("Premain called! ");
+        new AgentBuilder.Default()
+                .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
+                .type(ElementMatchers.nameStartsWith("com.tblf"))
+                .transform((builder, typeDescription, classLoader, javaModule)
+                        -> builder.method(ElementMatchers.any()).intercept(Advice.to(Interceptor.class)))
+                .installOn(instrumentation);
+    }
+
+    static class Interceptor {
+        @Advice.OnMethodEnter
+        static void enter(@Advice.Origin Method method) {
+            System.out.println("Im in "+method.toGenericString());
+        }
+
+        //called at the end of the method
+        @Advice.OnMethodExit
+        static void exit(@Advice.Origin Method method) {
+            System.out.println("Im out "+method.toGenericString());
+        }
     }
 }
